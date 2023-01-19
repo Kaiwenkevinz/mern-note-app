2be3753975a1f25ffa41e97d3e949c3e4a1411d2
Kaiwen Zhang <kevin.zhangkw@gmail.com>
2023-01-19 17:26:43 +0800

    vscode debug config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,20 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
-const app = express();
 
-const port = 3001;
+const app = express();
+const port = process.env.PORT;
 
 app.get("/", function (req, res) {
   res.send("Hello World");
   console.log("test1");
 });
 
-app.listen(port);
+app.listen(3001);
+
+// mongoose.connect(process.env.MONGO_CONNECTION_STRING!).then(() => {
+//   console.log("connected to MDB");
+// });
+
+
+// TODO: debug nodejs typescipt?
